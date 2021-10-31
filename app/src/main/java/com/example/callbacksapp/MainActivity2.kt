66e88d3a7493850a866663499798439966d6d1cc
Ahad <odisha2226@gmail.com>
2021-10-31 03:52:01 +0300

    update

--- app/src/main/java/com/example/callbacksapp/MainActivity2.kt
+++ app/src/main/java/com/example/callbacksapp/MainActivity2.kt
@@ -1,16 +1,26 @@
 package com.example.callbacksapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
 
+    lateinit var strt2Btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        strt2Btn= findViewById(R.id.ac2Btn)
 
         Toast.makeText(this, "Activity2 onCreate Called", Toast.LENGTH_SHORT).show()
+
+        strt2Btn.setOnClickListener {
+            val intent = Intent(this@MainActivity2 , MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
